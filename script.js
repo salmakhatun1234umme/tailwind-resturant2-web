ee74a11d61fecd5e31ddfc74745c1adfac3daedd
salmakhatun1234umme <ummesalma00569@gmail.com>
2024-12-21 11:02:15 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let main_button=document.getElementById('main_button');
+let main_menu=document.getElementById('main_menu');
+
+main_button.addEventListener('click',()=>{
+    main_menu.classList.toggle('hidden')
+})
